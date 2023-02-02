c2ddfb258ef8969b0c836589075b3148a4a6db3c
Nick <pretki123@ukr.net>
2023-02-01 22:03:28 -0700

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by Nick Pavlov on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
